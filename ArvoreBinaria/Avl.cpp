e7a3bb95101e4ec3010ae8814c85e16aa9ae2fe5
douglaslopesfarias@outlook.com <douglaslopesfarias@outlook.com>
2026-05-04 21:59:55 -0300

    correcao avl

--- ArvoreBinaria/Avl.cpp
+++ ArvoreBinaria/Avl.cpp
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct nodo
-{
+struct nodo{
 	int valor;
 	struct nodo *esq;
 	struct nodo *dir;
@@ -15,18 +16,18 @@ bool sair = false;
 int op;
 Nodo *raiz;
 
-void menu() {
-    printf("01 - Cira Raiz\n");
-    printf("02 - Insere nó \n");
-    printf("03 - Altura\n");
-    printf("04 - Imprimir\n"); 
-    printf("05 - Sair do Sistema\n");
-    printf("Digite uma opção: ");
+void menu(){
+	printf("01 - Criar a raiz\n");
+	printf("02 - Insere no\n");	
+	printf("03 - Altura \n");
+	printf("04 - Imprimir\n");
+	printf("05 - Sair do sistema\n");
+	printf("Digite uma opção:\n");
 }
 
-void sairDoSistema() {    
-    printf("\nSaindo do sistema...\n");
-    sair = true;
+void sairDoSistema(){
+	printf("Saindo do sistema...\n");
+	sair = true;
 }
 
 Nodo* create(int valor){
@@ -34,73 +35,79 @@ Nodo* create(int valor){
 	n->valor = valor;
 	n->esq = NULL;
 	n->dir = NULL;
+	n->altura = -1;
 	return n;
 }
 
-void imprimir(Nodo *n){
-	if(n==NULL)
+void imprimir(Nodo *n){	
+	if(n==NULL){
 		return;
+	}
 	imprimir(n->esq);
-	printf("\n%d",n->valor);
-	imprimir(n->dir);
+	printf("\n%d", n->valor);
+	imprimir(n->dir);	
 }
 
-void add(Nodo *n, int valor){	
-	if(valor < n->valor)	{
-		if(n->esq == NULL)
+
+void add(Nodo *n, int valor){
+	if(valor < n-> valor){
+		if(n->esq == NULL){
 			n->esq = create(valor);
-		else
+		}else{
 			add(n->esq,valor);
-	} else	{
-		if(n->dir == NULL)
+		}
+	}else{
+		if(n->dir == NULL){
 			n->dir = create(valor);
-		else
+		}else{
 			add(n->dir,valor);
+		}
 	}
 }
 
 void insere(){
 	int valor;
-	printf("Digite o valor do no: ");
-    scanf("%d", valor);
-    add(raiz, valor);	
+	printf("Digite o valor do no: \n");
+	scanf("%d", &valor);
+	add(raiz, valor);
 }
 
 void criaRaiz(){
 	int valorRaiz;
-	printf("Digite o valor da raiz: ");
-    scanf("%d", valorRaiz);
-	raiz = create(valorRaiz);	
+	printf("Digite o valor da raiz: \n");
+	scanf("%d", &valorRaiz);
+	raiz = create(valorRaiz);
+	system("pause");
 }
 
-int altura(Nodo *n){	
+
+int altura(Nodo *n){
 	if(n == NULL){
 		return -1;
 	}else{
-		int esq = altura(n->esq);
-		int dir = altura(n->dir);
-		if(esq > dir){
-			return esq + 1;
+		int alturaEsquerda = altura(n->esq);
+		int alturaDireita  = altura(n->dir);
+		if(alturaEsquerda > alturaDireita){
+			return alturaEsquerda + 1;
 		}else{
-			return dir + 1;
-		}
-	}	
-		
+			return alturaDireita + 1;
+		}		
+	}
 }
 
-void main(void) {	
-	while (sair == false) {
-        system("CLS");
-        menu();
-        scanf("%i", &op);
-        switch (op) {
-            case 1: criaRaiz(); break;
-            case 2: insere(); break;
-            case 3: printf("\nAltura: %d", altura(raiz)); break;
-            case 4: imprimir(raiz); break;            
-            case 5: sairDoSistema(); break;
-            default: printf("Opção inválida!\n"); system("pause");
-        }
-    }
-    
+int main(){
+	while(!sair){
+		system("CLS");
+		menu();
+		scanf("%d", &op);
+		switch(op){
+			case 1: criaRaiz(); break;
+			case 2: insere(); break;
+			case 3: printf("Altura: %d\n", altura(raiz)); system("pause");break;
+			case 4: imprimir(raiz);	printf("\n"); system("pause"); break;
+			case 5: sairDoSistema(); break;			
+			default: printf("Opção inválida!\n"); system("pause");
+		}
+	}
+	return 0;
 }
