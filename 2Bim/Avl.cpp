07d3303fb2cb6d7fd65e375c39635982c94c6746
douglaslopesfarias@outlook.com <douglaslopesfarias@outlook.com>
2026-05-13 19:06:19 -0300

    alteracao

--- 2Bim/Avl.cpp
+++ 2Bim/Avl.cpp
@@ -19,12 +19,11 @@ void menu(){
     if(raiz){
 	    printf("Valor da raiz: %d", raiz->valor);
 	    printf("\n");
-	}
-	printf("01 - Criar a raiz\n");
-	printf("02 - Insere no\n");	
-	printf("03 - Altura \n");
-	printf("04 - Imprimir\n");
-	printf("05 - Sair do sistema\n");
+	}	
+	printf("01 - Insere\n");	
+	printf("02 - Altura \n");
+	printf("03 - Imprimir\n");
+	printf("04 - Sair do sistema\n");
 	printf("Digite uma opção:\n");
 	
 }
@@ -39,7 +38,7 @@ Nodo* create(int valor){
 	n->valor = valor;
 	n->esq = NULL;
 	n->dir = NULL;
-	n->altura = -1;
+	n->altura = 0;
 	return n;
 }
 
@@ -71,27 +70,24 @@ void add(Nodo *n, int valor){
 
 void insere(){
 	int valor;
-	printf("Digite o valor do no: \n");
+	printf("Digite o valor: \n");
 	scanf("%d", &valor);
-	add(raiz, valor);
-}
-
-void criaRaiz(){
-	int valorRaiz;
-	printf("Digite o valor da raiz: \n");
-	scanf("%d", &valorRaiz);
-	raiz = create(valorRaiz);
+	if(raiz==NULL){
+		raiz = create(valor);
+		printf("Raiz criada com sucesso!\n");		
+	}else{
+		add(raiz, valor);
+		printf("No adicionado com sucesso!\n");			
+	}
 	system("pause");
 }
-
 
 int altura(Nodo *n){
 	if(n == NULL){
 		return -1;
 	}else{
 		int alturaEsquerda = altura(n->esq);
-		printf("Valor: %d", n->valor);
-		
+		printf("Valor: %d", n->valor);		
 		printf("\nvalor da esquerda: %d", alturaEsquerda);
 		printf("\n");
 		int alturaDireita  = altura(n->dir);
@@ -111,17 +107,55 @@ int altura(Nodo *n){
 	}
 }
 
+short maiorAltura(short esq, short dir){
+	return (esq>dir) ? esq : dir;
+}
+
+short alturaDoNo(Nodo *no){
+	if(no == NULL){
+		return -1;
+	}else{
+		return no->altura;
+	}
+}
+
+short fatorDeBalancemento(Nodo *no){
+	if(no){
+		return (alturaDoNo(no->esq) - alturaDoNo(no->dir));
+	}else{
+		return 0;
+	}
+}
+
+Nodo* rotacaoEsquerda(Nodo *r){
+	Nodo *y, *f;
+	
+	y = r->dir;
+	f = y->esq;
+	
+	y->esq = r;
+	r->dir = f;
+	
+	r->altura = maiorAltura(alturaDoNo(r->esq), alturaDoNo(r->dir)) + 1;
+	y->altura = maiorAltura(alturaDoNo(y->esq), alturaDoNo(y->dir)) + 1;
+	
+	return y;
+}
+
+
+
+
+
 int main(){
 	while(!sair){
 		system("CLS");
 		menu();
 		scanf("%d", &op);
-		switch(op){
-			case 1: criaRaiz(); break;
-			case 2: insere(); break;
-			case 3: printf("Altura: %d\n", altura(raiz)); system("pause");break;
-			case 4: imprimir(raiz);	printf("\n"); system("pause"); break;
-			case 5: sairDoSistema(); break;			
+		switch(op){			
+			case 1: insere(); break;
+			case 2: printf("Altura: %d\n", altura(raiz)); system("pause");break;
+			case 3: imprimir(raiz);	printf("\n"); system("pause"); break;
+			case 4: sairDoSistema(); break;			
 			default: printf("Opção inválida!\n"); system("pause");
 		}
 	}
